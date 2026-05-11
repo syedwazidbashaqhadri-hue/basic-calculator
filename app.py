1dd67500956cfa3e89f8df55f852e37158897244
manoj <syedwazidbashaqhadri>
2026-05-11 17:23:33 +0530

    final version

--- app.py
+++ app.py
@@ -3,25 +3,29 @@ import streamlit as st
 
 st.title("calculator of two numbers")
 
-
 #n1=int(input('enter the first number:'))
 n1=int(st.number_input("enter the first number:"))
 #n2=int(input('enter the second number:'))
-n2=int(st.number_input("enter the second number:")
+n2=int(st.number_input("enter the second number:"))
        
-operator=st.selectbox("operation",["multiplication","addition","subtraction","power/exponentiation","division","floor division"]))
+operator=st.selectbox("operation",["multiplication","addition","subtraction","power/exponentiation","division","floor division"])
 
-if operator=='multiplication':
-    st.write(multiply(n1,n2))
-elif operator=='addition':
-    st.write(add(n1,n2))
-elif operator=='subtraction':
-    st.write(sub(n1,n2))
-elif operator=='power/exponentiation':
-    st.write(power(n1,n2))
-elif operator=='division':
-    st.write(divide(n1,n2))
-elif operator=='floor division':
-    st.write(floor(n1,n2))
-else:
-    st.write("please select a valid option")
+#a=st.button('click')
+#st.write(a)
+
+if st.button("show result"):
+
+    if operator=='multiplication':
+        st.write(multiply(n1,n2))
+    elif operator=='addition':
+        st.write(add(n1,n2))
+    elif operator=='subtraction':
+        st.write(sub(n1,n2))
+    elif operator=='power/exponentiation':
+        st.write(power(n1,n2))
+    elif operator=='division':
+        st.write(divide(n1,n2))
+    elif operator=='floor division':
+        st.write(floor(n1,n2))
+    else:
+        st.write("please select a valid option")
