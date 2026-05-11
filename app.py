88eb440c9d000678d9f01d6ee54905f4f5e63b1c
manoj <syedwazidbashaqhadri>
2026-05-11 16:55:11 +0530

    streamlit version 1

--- app.py
+++ app.py
@@ -7,31 +7,21 @@ st.title("calculator of two numbers")
 #n1=int(input('enter the first number:'))
 n1=int(st.number_input("enter the first number:"))
 #n2=int(input('enter the second number:'))
-n2=int(st.number_input("enter the second number:"))
+n2=int(st.number_input("enter the second number:")
+       
+operator=st.selectbox("operation",["multiplication","addition","subtraction","power/exponentiation","division","floor division"]))
 
-st.write("select the operation to do in the two numbers ")
-st.write("type 1- multiplication")
-st.write("type 2- addition")
-st.write("type 3- subtraction")
-st.write("type 4- power/exponentiation ")
-st.write("type 5- division")
-st.write("type 6- floor division")
-
-
-#operator=int(input("enter the option of the above:")) 
-operator=int(st.number_input("enter the option of the above:")) 
-
-if operator==1:
+if operator=='multiplication':
     st.write(multiply(n1,n2))
-elif operator==2:
+elif operator=='addition':
     st.write(add(n1,n2))
-elif operator==3:
+elif operator=='subtraction':
     st.write(sub(n1,n2))
-elif operator==4:
+elif operator=='power/exponentiation':
     st.write(power(n1,n2))
-elif operator==5:
+elif operator=='division':
     st.write(divide(n1,n2))
-elif operator==6:
+elif operator=='floor division':
     st.write(floor(n1,n2))
 else:
     st.write("please select a valid option")
